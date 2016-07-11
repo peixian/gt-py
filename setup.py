ad9995c6db369f0358464a4eddc92f483b43e15c
Peixian Wang <peixian@fastmail.com>
2016-07-11 13:03:53 -0500

    Submit to pip

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 
+def readme():
+    with open('readme.md') as f:
+        return f.read()
+
 setup(
     name='gt-py',
-    packages=['govtrack'],
+    packages=['gt-py'],
     install_requires=['unittest2==1.1.0', 'requests==2.9.1'],
-    version='0.1',
+    version='1.0',
     description='Python library for GovTrack.us API',
     author='Peixian Wang',
     author_email='peixian@driftingicebergs.com',
     url='https://github.com/peixian/gt-py',
-    download_url='',
+    download_url='https://github.com/peixian/gt-py/archive/1.0.tar.gz',
     keywords=['api', 'python', 'govtrack', 'gt'],
     classifiers=[],
     test_suite='nose.collector',
